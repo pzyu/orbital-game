f8c48da6672032bbfebef32c17acd2f2524e0567
oqyxxy <oqyxxy93@gmail.com>
2016-07-24 22:00:22 +0800

    - revamped AI logic to now perform greedy algo target search - Single player enemies are now randomized! - modularized a few AI functions for better control.

--- states/HeroBase.js
+++ states/HeroBase.js
@@ -124,7 +124,7 @@ BasicGame.HeroBase = function (id, game, x, y, sprite, team, nick) {
 	BasicGame.playerCG.add(this);
 
 	// Multiplayer stuff
-	if (id == "SoloKid" || id.substring(0, 10) == "retard_Bot") {
+	if (id == "SoloKid" || id.substring(0, 6) == "AI_Bot") {
 		this.refMP = this.game.state.states['MainGame'];
 	} else if (id == "TutorialPlayer" || id.substring(0, 12) == "tutorial_Bot") {
 		this.refMP = this.game.state.states['Tutorial'];
@@ -356,19 +356,27 @@ BasicGame.HeroBase.prototype.getHit = function(damage, knockbackX, knockbackY, k
 					// player has been defeated.
 					this.refMP.winGame();
 				} else {
-					//onLevelUp(this); // bot becomes stronger
-					if (this.refMP.teamScores[1] == 5 && this.refMP.playerList["retard_Bot2"] == null) {
-						// spawn new enemy (Disruptor)
-						this.refMP.spawnAI("retard_Bot2", 500, 1000, "player_gunner", "Enemy Disruptor(AI)", 2, 3);
-						this.refMP.broadcast("Enemy support has arrived!", 2);
-					} else if (this.refMP.teamScores[1] == 10 && this.refMP.playerList["retard_Bot3"] == null) {
-						// spawn new enemy (Disruptor)
-						this.refMP.spawnAI("retard_Bot3", 500, 1000, "player_destroyer", "Enemy Destroyer(AI)", 2, 5);
-						this.refMP.broadcast("Destroyer, incoming!", 2);
-					} else if (this.refMP.teamScores[1] == 15 && this.refMP.playerList["retard_Bot4"] == null) {
-						// spawn new enemy (Disruptor)
-						this.refMP.spawnAI("retard_Bot4", 500, 1000, "player_trooper", "Enemy Ace(AI)", 2, 7);
-						this.refMP.broadcast("The enemy has brought out their Ace, good luck surviving.", 2);
+					var enemyCount = 0;
+					for (var idx in this.refMP.playerList) {
+						if (this.refMP.playerList[idx][2] == 2) {
+							enemyCount++;
+						}
+					}
+					if (this.refMP.teamScores[1] == 5 && enemyCount == 1) {
+						// spawn new enemy (2nd)
+						this.refMP.spawnRandomAIFromList(3);
+						//this.refMP.spawnAI("AI_Bot2", 500, 1000, "player_gunner", "Enemy Disruptor(AI)", 2, 3);
+						//this.refMP.broadcast("Enemy support has arrived!", 2);
+					} else if (this.refMP.teamScores[1] == 10 && enemyCount == 2) {
+						// spawn new enemy (3rd)
+						this.refMP.spawnRandomAIFromList(5);
+						//this.refMP.spawnAI("AI_Bot3", 500, 1000, "player_destroyer", "Enemy Destroyer(AI)", 2, 5);
+						//this.refMP.broadcast("Destroyer, incoming!", 2);
+					} else if (this.refMP.teamScores[1] == 15 && enemyCount == 3) {
+						// spawn new enemy (4th)
+						this.refMP.spawnRandomAIFromList(7);
+						//this.refMP.spawnAI("AI_Bot4", 500, 1000, "player_trooper", "Enemy Ace(AI)", 2, 7);
+						//this.refMP.broadcast("The enemy has brought out their Ace, good luck surviving.", 2);
 					}
 				}
 				// Calculate exp to credit to the killer based on killer's hero level and level difference.
